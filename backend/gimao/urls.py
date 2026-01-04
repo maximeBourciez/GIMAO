5e25129c8f2455adf76fd81796fc4143233a6296
Maxime BOURCIEZ <maxime.bourciez@gmail.com>
2026-01-04 14:39:56 +0100

    feat (django): Ajout d'une page permettant les redirections quand on oublie /admin, /swagger en accès au back

--- backend/gimao/urls.py
+++ backend/gimao/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from gimao.views import home_view
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -20,6 +21,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     
     # Swagger / API Documentation
