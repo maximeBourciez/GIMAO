5369e41d0171517808ee0cb4bb44ffa8829b1775
Maxime BOURCIEZ <maxime.bourciez@gmail.com>
2026-01-27 11:47:53 +0100

    fix (update-bt): Filtrage des status des BT récupérés afin de rendre les nombres + parlants

--- backend/tasks/updateBtStatus.py
+++ backend/tasks/updateBtStatus.py
@@ -13,7 +13,11 @@ def update_bt_status():
     au statut 'En retard' si leur date prévue est dépassée.
     """ 
 
-    bons_travail = BonTravail.objects.filter(date_prevue__lt=timezone.now(), date_debut__isnull=True)
+    bons_travail = BonTravail.objects.filter(
+        date_prevue__lt=timezone.now(),
+        date_debut__isnull=True,
+        statut__notin=['EN_RETARD', 'TERMINE', 'CLOTURE']
+    )
     updated_bts = 0
     for bt in bons_travail:
         bt.statut = 'EN_RETARD'
