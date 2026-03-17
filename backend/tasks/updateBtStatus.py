a891187e57dd8777bef19ad1f5a9114b7a6747aa
aberho001 <ando.berho@gmail.com>
2026-03-17 09:35:48 +0100

    fix (test): updateBtStatus change notin en exclude in

--- backend/tasks/updateBtStatus.py
+++ backend/tasks/updateBtStatus.py
@@ -14,11 +14,11 @@ def update_bt_status():
     """ 
 
     bons_travail = BonTravail.objects.filter(
-        date_prevue__lt=timezone.now(),
-        date_debut__isnull=True,
-        archive=False,
-        statut__notin=['EN_RETARD', 'TERMINE', 'CLOTURE']
-    )
+    date_prevue__lt=timezone.now(),
+    date_debut__isnull=True,
+    archive=False,
+    ).exclude(statut__in=['EN_RETARD', 'TERMINE', 'CLOTURE'])
+    
     updated_bts = 0
     for bt in bons_travail:
         bt.statut = 'EN_RETARD'
