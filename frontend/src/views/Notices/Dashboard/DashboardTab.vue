8ed452f4221f2cca54b311a7601c3afd0dec6db8
Maxime BOURCIEZ <maxime.bourciez@gmail.com>
2026-03-17 08:08:59 +0100

    feat (notices): ajout des tabs dans la vue opérateur

---
+++ frontend/src/views/Notices/Dashboard/DashboardTab.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="text-body-1 font-weight-bold mb-2">
+      Votre tableau de bord
+    </div>
+
+    <div class="text-body-2">
+      - À la connexion, votre tableau de bord affiche :<br />
+      &nbsp;&nbsp;• <strong>Vos demandes d'intervention</strong> : celles que vous avez créées, avec leur état actuel.<br />
+      &nbsp;&nbsp;• <strong>Liste des équipements</strong> du parc que vous pouvez consulter.<br />
+      - Des statistiques sur vos demandes sont affichées en haut de page.
+    </div>
+  </div>
+</template>
