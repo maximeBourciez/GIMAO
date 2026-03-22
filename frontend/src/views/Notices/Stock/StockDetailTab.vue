5308c6f3c2ccd6329610c09c71814117ade44b9e
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2026-03-22 01:09:33 +0100

    feat (notice) : ajout des screens manquants pour la notice technicien et refonte tab stocks

--- frontend/src/views/Notices/Stock/StockDetailTab.vue
+++ frontend/src/views/Notices/Stock/StockDetailTab.vue
@@ -3,20 +3,37 @@
     <h4>Détails et traçabilité d'un consommable</h4>
 
     <div class="text-body-2 mb-4 mt-2">
-      En cliquant sur une pièce spécifique depuis la liste des stocks, vous accédez à sa <strong>fiche détaillée</strong>. Cette fiche centralise toutes les opérations effectuées sur ce produit, assurant ainsi une traçabilité complète de son utilisation dans l'entreprise.
+      En cliquant sur une pièce spécifique depuis la liste des stocks, vous accédez à sa <strong>fiche détaillée</strong>. Cette fiche centralise les informations essentielles à la gestion de la pièce.
       <br /><br />
 
       <strong>Contenu de la fiche d'un consommable :</strong>
       <ul class="ml-4 mt-2 mb-4">
-        <li><strong>Informations générales :</strong> la référence exacte de l'article, sa description détaillée, ainsi que sa valorisation financière.</li>
-        <li><strong>État et répartition du stock :</strong> au-delà de la quantité physiquement disponible, vous pouvez y apercevoir les pièces en cours de réapprovisionnement, ou encore celles qui sont "réservées" par des techniciens pour des Bons de Travail à venir.</li>
+        <li><strong>Section Informations :</strong> récapitule les données globales comme le nom du consommable, le seuil de stock faible pour les alertes, ainsi que la quantité totale disponible.</li>
+        <li><strong>Section Stocks en magasin :</strong> présente sous forme de tableau la répartition du matériel, détaillant pour chaque magasin concerné la quantité présente physiquement.</li>
+        <li><strong>Section Historique des achats :</strong> permet de retracer les approvisionnements via un tableau indiquant la date, le fournisseur, le nom du fabriquant, la quantité commandée, le prix unitaire et le prix total.</li>
       </ul>
 
     </div>
+    <ZoomImage :src="require('@/assets/images/notices/stocks/detail-stock-technicien.png')" alt="Détail des stocks" v-if="role === 'Technicien'"/>
+
   </v-container>
 </template>
 
 <script setup>
+import ZoomImage from "../common/ZoomImage.vue";
+
+const props = defineProps({
+  role: {
+    type: String,
+    default: "Technicien"
+  }
+});
+
+const roles = ["Technicien", "Magasinier", "Responsable GMAO"];
+
+const roleIsAbove = (minRole) => {
+  return roles.indexOf(props.role) >= roles.indexOf(minRole);
+};
 </script>
 
 <style scoped>
--- frontend/src/views/Notices/Stock/StockListTab.vue
+++ frontend/src/views/Notices/Stock/StockListTab.vue
@@ -3,25 +3,24 @@
     <h4>Consulter la liste des stocks</h4>
 
     <div class="text-body-2 mb-4 mt-2">
-      La rubrique <strong>Stocks</strong> offre une vue globale sur l'ensemble des pièces détachées et consommables de l'entreprise. Cette liste vous permet d'anticiper vos besoins ou de vérifier les disponibilités.
+      La rubrique <strong>Stocks</strong> offre une vue globale sur l'ensemble des pièces détachées et consommables. Cette liste vous permet d'anticiper vos besoins ou de vérifier les disponibilités.
       <br /><br />
 
       <strong>Informations disponibles dans la liste :</strong>
       <ul class="ml-4 mt-2 mb-4">
-        <li><strong>Désignation :</strong> le nom et la référence interne des différentes pièces.</li>
-        <li><strong>Quantité disponible :</strong> le niveau actuel des stocks physiques, vous indiquant en temps réel s'il y a assez de matériel pour mener à bien de futures interventions.</li>
-        <li><strong>Informations complémentaires :</strong> données générales sur le prix unitaire ou la valeur globale des éléments.</li>
+        <li><strong>Statistiques :</strong> des indicateurs visuels du niveau global affichant les consommables hors stock, sous le seuil critique, et ceux avec un stock suffisant.</li>
+        <li><strong>Détails par consommable :</strong> pour chaque élément de la liste, vous retrouvez directement le nom du consommable, le magasin où il est localisé, ainsi que la quantité disponible.</li>
       </ul>
 
       <strong>Droits et permissions :</strong>
       <ul class="ml-4 mt-2 mb-4">
         <li><strong>Consultation :</strong> vous pouvez à tout moment vérifier la présence d'une pièce et son niveau de stock en lisant simplement cette liste.</li>
-        <li v-if="!roleIsAbove('Magasinier')"><strong>Modification :</strong> en tant que technicien, toute modification manuelle des quantités du stock vous est impossible depuis cet onglet. L'entrée de nouvelles quantités en stock ou l'ajustement manuel ne peuvent être effectués que par le Magasinier ou le Responsable. Le technicien n'influence le stock que par l'utilisation de la fonctionnalité "Demande de consommables" dans une intervention.</li>
+        <li v-if="!roleIsAbove('Magasinier')"><strong>Modification :</strong> en tant que technicien, toute modification manuelle des quantités du stock vous est impossible. L'entrée de nouvelles quantités en stock ou l'ajustement manuel ne peuvent être effectués que par le magasinier ou le responsable. Le technicien n'influence le stock que par l'utilisation de consommables lors d'une intervention.</li>
         <li v-else><strong>Modification :</strong> en tant que Magasinier ou Responsable, vous avez les permissions nécessaires pour ajouter manuellement des pièces ou ajuster les niveaux d'un produit (entrée, sortie, erreur d'inventaire).</li>
       </ul>
     </div>
 
-    <ZoomImage :src="require('@/assets/images/notices/stocks/liste-technicien-stock.png')" alt="Liste des stocks" v-if="role === 'Technicien'"/>
+    <ZoomImage :src="require('@/assets/images/notices/stocks/liste-stock-technicien.png')" alt="Liste des stocks" v-if="role === 'Technicien'"/>
   </v-container>
 </template>
 
@@ -35,7 +34,7 @@ const props = defineProps({
   }
 });
 
-const roles = ["Opérateur", "Technicien", "Magasinier", "Responsable GMAO"];
+const roles = ["Technicien", "Magasinier", "Responsable GMAO"];
 
 const roleIsAbove = (minRole) => {
   return roles.indexOf(props.role) >= roles.indexOf(minRole);
