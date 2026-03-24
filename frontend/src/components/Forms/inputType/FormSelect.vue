771b98eefcee487201308b3b5fb86f93ac8a4695
Maxime BOURCIEZ <maxime.bourciez@gmail.com>
2026-03-24 10:48:27 +0100

    feat (Formselect): ajout de la possibilité de le disable

--- frontend/src/components/Forms/inputType/FormSelect.vue
+++ frontend/src/components/Forms/inputType/FormSelect.vue
@@ -38,7 +38,8 @@ import { computed, inject } from 'vue';
 const props = defineProps({
     label: { type: String, default: '' },
     fieldName: { type: String, required: true },
-    step: { type: Number, default: null }
+    step: { type: Number, default: null },
+    disabled: { type: Boolean, default: false }
 });
 
 const validation = inject('validation', null);
