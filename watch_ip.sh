8e5ef027569df48a3fb2bd448b91cec57e8f74b6
Iban ARANDIA <arandiaiban@gmail.com>
2026-03-26 15:14:59 +0100

    feat (prod) : auto detecte le changement d'ip et relance le docker

---
+++ watch_ip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+CURRENT_IP=$(hostname -I | awk '{print $1}')
+
+echo "IP initiale : $CURRENT_IP"
+
+while true; do
+  sleep 60
+  
+  NEW_IP=$(hostname -I | awk '{print $1}')
+  
+  if [ "$NEW_IP" != "$CURRENT_IP" ] && [ ! -z "$NEW_IP" ]; then
+    echo "[$(date)] $CURRENT_IP -> $NEW_IP"
+    CURRENT_IP=$NEW_IP
+    ./init.sh
+  fi
+done
